e3712d533803c7236fb6b9488a2d6684e1dbb2a2
Jesica Guallpa <guallpajesica65@gmail.com>
2025-10-01 13:41:51 -0700

    Wave 5 All Tests Passed

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,18 @@
 from .student import Student
 
-# add MiddleSchoolStudent here
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, gets_transportation=False):
+        super().__init__(name, grade, classes)
+        self.gets_transportation = gets_transportation
+
+    def display_transport_message(self):
+        has_transport = "has" if self.gets_transportation else "doesn't have"
+
+        return f"{self.name} {has_transport} transport privileges"
+
+    # overrides the Student class's summary
+    def summary(self):
+        student_summary = super().summary()
+        transport_message = self.display_transport_message()
+        return "\n".join((student_summary,transport_message))
+
